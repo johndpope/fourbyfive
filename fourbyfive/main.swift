6e4a964450a8561d750fa61d19c0351013111122
mwielocha <mwielocha@icloud.com>
2018-12-04 21:28:51 +0100

    project setup

--- fourbyfive/main.swift
+++ fourbyfive/main.swift
@@ -7,6 +7,22 @@
 //
 
 import Foundation
+import Alamofire
 
 print("Hello, World!")
 
+let semaphore = DispatchSemaphore(value: 0)
+
+let queue = DispatchQueue(label: "com.test.api", qos: .background, attributes: .concurrent)
+
+Alamofire.request("https://httpbin.org/get")
+    .responseJSON(queue: queue) { response in
+        if let json = response.result.value {
+            print(json)
+        }
+        semaphore.signal()
+}
+
+_ = semaphore.wait(timeout: DispatchTime.distantFuture)
+
+print("Done!")
